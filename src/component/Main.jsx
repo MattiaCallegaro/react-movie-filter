752699e1e8a65a911b8c336ee41e58efd7704e88
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-12 15:17:31 +0200

    [UPD] Add select with films genre

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -3,29 +3,37 @@ import initialFilms from './data/Films'
 
 function Main() {
     return (
-        <>
-            <div className="container">
-                <table class="table">
 
-                    <thead >
-                        <tr>
-                            <th scope="col">Film</th>
-                            <th scope="col">Genere</th>
+        <div className="container">
+            <select className="form-select" aria-label="Default select example">
+                <option value="" selected disabled>Seleziona il genere del film</option>
+                <option value="1">Fantascienza</option>
+                <option value="2">Thriller</option>
+                <option value="3">Romantico</option>
+                <option value="4">Azione</option>
+                <option value="5">Fantascienza</option>
+                <option value="6">Thriller</option>
+            </select>
+            <table class="table">
+                <thead >
+                    <tr>
+                        <th scope="col">Film</th>
+                        <th scope="col">Genere</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider" >
+                    {initialFilms.map((film, index) => (
+                        <tr key={index}>
+                            <td>{film.title}</td>
+                            <td>{film.genre}</td>
                         </tr>
-                    </thead>
-                    <tbody class="table-group-divider" >
-                        {initialFilms.map((film, index) => (
-                            <tr key={film.index}>
-                                <td>{film.title}</td>
-                                <td>{film.genre}</td>
-                            </tr>
 
 
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
     )
 }
 
