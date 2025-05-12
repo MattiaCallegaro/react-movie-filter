b183eb79f75dfbd0247b1a62f9e161fdf78177c5
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-12 14:32:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './component/Header'
+import Main from './component/Main'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export default App
