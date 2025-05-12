1268645f094bf2420719c44503828ef84666a139
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-12 16:21:22 +0200

    [Add} add filter genre

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -3,18 +3,35 @@ import initialFilms from './data/Films'
 import { useState, useEffect } from 'react'
 
 function Main() {
-
+    // stato con i dati dei film presi dall'array è read-only 
     const [films] = useState(initialFilms)
+    //lo stato per i generi di select
     const [selectedGenre, setSelectedGenre] = useState("")
+    //lo stato per i generi nella tabella
     const [filteredFilms, setFilteredFilms] = useState(initialFilms)
 
 
-    //filtro i film in base al termine di ricerca
+    //filtro i film in base al termine di ricerca usando useEffect con filter
+    useEffect(() => {
+        if (!selectedGenre) {
+            setFilteredFilms(films)
+        }
+        else {
+            const filtered = films.filter(film =>
+                film.genre.toLowerCase() === selectedGenre.toLocaleLowerCase()
+            )
+            setFilteredFilms(filtered)
+        }
+        //dipendenza
+    }, [selectedGenre, films])
 
     return (
 
         <div className="container">
-            <select className="form-select" >
+            <select className="form-select"
+                value={selectedGenre}
+                onChange={(e) => selectedGenre(e.target.value)}
+            >
                 <option value="">Seleziona il genere del film</option>
                 <option value="1">Fantascienza</option>
                 <option value="2">Thriller</option>
